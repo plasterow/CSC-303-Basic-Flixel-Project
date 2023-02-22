7e2204a9cb42154600a14264742dc29455ca7033
Plaster <op130@live.missouristate.edu>
2023-02-21 18:15:36 -0600

    Working now, Part 2 done

--- source/ScreensaverHero.hx
+++ source/ScreensaverHero.hx
@@ -1,24 +1,27 @@
 package;
 
+import flixel.FlxG;
 import flixel.util.FlxColor;
 import flixel.FlxSprite;
 import Math;
 
 class ScreensaverHero extends FlxSprite 
 {
+    private var MovementDirectionX = FlxG.random.float(-2, 2);
+    private var MovementDirectionY = FlxG.random.float(-2, 2);
+    
     public function new()
     {
         super();
-        makeGraphic(50, 80, FlxColor.WHITE, false);        
+        makeGraphic(50, 80, FlxColor.WHITE, false);
     }
     
     override public function update(elapsed:Float) 
     {
-        var X = this.x;
-        var Y = this.y;
-        var ScreenPos = X + Y;
-        this.color = FlxColor.fromInt(Math.floor(ScreenPos));
+        this.color = FlxColor.fromInt(Math.floor(this.x + this.y));
 
+        this.x += MovementDirectionX;
+        this.y += MovementDirectionY;
         super.update(elapsed);
     }
 }
