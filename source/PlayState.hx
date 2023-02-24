831c6aae26897a8f2c0bfdb283b4e86d16a1804b
plasterow <121645855+plasterow@users.noreply.github.com>
2023-02-24 09:57:33 -0600

    Update PlayState.hx
    Added a loop for creating the ScreensaverHero classes. While it didn't seem that bad to just copy and paste the original code, it does end up being a big block that could easily be reduced to just a few lines. Thanks!

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -16,20 +16,11 @@ class PlayState extends FlxState
 		text.screenCenter();
 		add(text);
 
-		var hero1 = new ScreensaverHero();
-		var hero2 = new ScreensaverHero();
-		var hero3 = new ScreensaverHero();
-		var hero4 = new ScreensaverHero();
-
-		hero1.screenCenter();
-		hero2.screenCenter();
-		hero3.screenCenter();
-		hero4.screenCenter();
-
-		add(hero1);
-		add(hero2);
-		add(hero3);
-		add(hero4);
+		for (i in 0...4) {
+			var hero = new ScreensaverHero();
+			hero.screenCenter();
+			add(hero);
+		}
 
 		var ControllableHero1 = new ControllableHero();
 		add(ControllableHero1);
